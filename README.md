bb4961581f7523108fc69f0a2caed0c7f3db181e
KP | Venk0x. 1991 <111184666+DUHOKDUHOK@users.noreply.github.com>
2022-10-31 21:28:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# YOUTUBE-AGADARY BY VENK0X 1991
+
+ئاگاداری یوتوب
+
+#heroku
+heroku.com
+![Preview](https://cdn.discordapp.com/attachments/833871627087118397/848531462361972766/DisTube.png)
+
+#arbele
---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: "toke Bot Lera Dane",
+    channel: "id Channel lera Dane ",
+    messageTemplate: "Slaw @everyone, **{author}** Vedio nwe bllawkrayawa✅ **{title}**!\n{url}",
+    channel_id: "UCFbWvTkRe8Fq2aXmnIvLp9A",
+    watchInterval: 30000
+}
