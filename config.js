879f967b218ab2340d5be314114442db05984b1c
SHEXWLATT <113339116+SHEXWLATT@users.noreply.github.com>
2022-11-10 20:12:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "OTA0MzY4MDk3ODY2MTc0NTA1.GFZ72g.KgsX29mKCLbnVOyW2gA3rXnufen-_b_5YjgAUo",
+    token: "OTA0MzY4MDk3ODY2MTc0NTA1.GMlI16.RBAtAdqI7jV8LGwOqXPAuDz9HPtaYo2qATzDZg",
     channel: "1009303218670555228",
     messageTemplate: "Slaw @everyone, **{author}** Vedio nwe bllawkrayawa✅ **{title}**!\n{url}",
     channel_id: "https://www.youtube.com/@SHEXWLAT",
